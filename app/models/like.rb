8ef5c70bdabebbbbc234e7ed44f7479f0c9b880e
hxie0 <xhy@chicagobooth.edu>
2021-08-12 22:20:31 +0000

    version 1.0.0

---
+++ app/models/like.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  fan_id     :integer
+#  photo_id   :integer
+#
+class Like < ApplicationRecord
+  validates(:user_id, { :presence => true })
+  validates(:photo_id, { :presence => true })
+  validates(:photo_id, { :uniqueness => { :scope => ["user_id"], :message => "already liked" } })
+
+  # Add Direct Associations
+  belongs_to(:user, { :required => true, :class_name => "User", :foreign_key => "fan_id" })
+  belongs_to(:photo, { :required => true, :class_name => "Photo", :foreign_key => "photo_id", :counter_cache => true })
+end
---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  comments_count  :integer
+#  email           :string
+#  likes_count     :integer
+#  password_digest :string
+#  private         :boolean
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  validates(:username, { :presence => true })
+  validates(:username, { :uniqueness => true })
+  validates :email, :presence => true
+  validates :email, :uniqueness => { :case_sensitive => false }
+  has_secure_password
+
+  # Add Direct Associations
+  has_many(:likes, { :class_name => "Like", :foreign_key => "fan_id", :dependent => :destroy })
+  has_many(:comments, { :class_name => "Comment", :foreign_key => "author_id", :dependent => :destroy })
+  has_many(:sent_follow_requests, { :class_name => "FollowRequest", :foreign_key => "sender_id", :dependent => :destroy })
+  has_many(:received_follow_requests, { :class_name => "FollowRequest", :foreign_key => "recipient_id", :dependent => :destroy })
+  has_many(:own_photos, { :class_name => "Photo", :foreign_key => "owner_id", :dependent => :destroy })
+
+  # Add Indirect Associations (Guide)
+  has_many(:following, { :through => :sent_follow_requests, :source => :recipient })
+  has_many(:followers, { :through => :received_follow_requests, :source => :sender })
+  has_many(:liked_photos, { :through => :likes, :source => :photo })
+  has_many(:feed, { :through => :following, :source => :own_photos })
+  has_many(:activity, { :through => :following, :source => :liked_photos })
+end
